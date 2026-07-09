9e92a487a4712e88eaa4dba8a675cf0fb7ca02c8
Akshay301A <akshayraj20031430@gmail.com>
2026-07-09 11:31:30 +0530

    allow packaged desktop app origin

--- app/main.py
+++ app/main.py
@@ -42,7 +42,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=settings.cors_origins,
+        allow_origins=list(dict.fromkeys([*settings.cors_origins, "ceaser-app://bundle"])),
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
